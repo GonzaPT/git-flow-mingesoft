c8cd7e223b7ae3a0c2ff08f788fcb0670f57d45f
Gonzalo Pulgar <gonzalo.pulgar@usach.cl>
2022-04-19 23:20:37 -0400

    se agrego version a la pagina principal

--- src/main/java/pep/mingesoft/demo/helloWorld.java
+++ src/main/java/pep/mingesoft/demo/helloWorld.java
@@ -10,7 +10,8 @@ public class helloWorld {
         public String holaMundo() {
                 int b = 3;
                 int c = 7;
-                return "Hola Mundo, soy una aplicacion web super simple: " + (b + 4 * 7 - c);
+                String var = "Version";
+                return "Hola Mundo, soy una aplicacion web super simple: " + (b + 4 * 7 - c) + var;
         }
 
 }
